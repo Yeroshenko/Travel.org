f2f2451808bbf726974410d17ade8787363c3280
Valeriy Yeroshenko <valeriy.yeroshenko@gmail.com>
2019-09-19 18:07:12 +0300

    Made a countdown timer

--- js/script.js
+++ js/script.js
@@ -35,4 +35,57 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
   });
+
+  // Timer
+
+  let deadline = '2019-9-20';
+
+  function getTimeRemaining(endTime) {
+    let t = Date.parse(endTime) - Date.parse(new Date()),  //  t = time difference
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/1000/60/60));
+
+    return {
+      'total'   : t,
+      'hours'   : hours,
+      'minutes' : minutes,
+      'seconds' : seconds
+    }; 
+  }
+
+  function setClock(id, endTime) {
+    let timer        = document.getElementById(id),
+        hours        = timer.querySelector('.hours'),
+        minutes      = timer.querySelector('.minutes'),
+        seconds      = timer.querySelector('.seconds'),
+        timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+      let t = getTimeRemaining(endTime);
+      
+      hours.textContent   = ('0' + t.hours).slice(-2);
+      minutes.textContent = ('0' + t.minutes).slice(-2);
+      seconds.textContent = ('0' + t.seconds).slice(-2);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+        hours.textContent   = '00';
+        minutes.textContent = '00';
+        seconds.textContent = '00';
+      }
+    }
+  }
+
+  setClock('timer', deadline);
+
+
+
+
+
+
+
+
+
+
 });
